c5684941353e33b47d138ca04bf31f74c5a43289
Paul de Vries <paul@qaraqter.nl>
2014-12-15 16:54:47 +0100

    Update angular-sizzle.js, copy objects from element._element to the new angular.element

--- angular-sizzle.js
+++ angular-sizzle.js
@@ -1,45 +1,58 @@
 (function (angular, sizzle) {
 	'use strict';
-	
-	var orginal = '_element',
-		sizzle = 'element';
-	
-	if(angular && sizzle) {
-	
-		angular[orginal] = angular.element;
-		angular[sizzle] = function ( selector ) {
 
-			if ( selector instanceof angular[orginal] ) {
+	var oldElement = '_element',
+		newElement = 'element';
+
+	if(angular && sizzle) {
+
+		// Replace 'angular._element' with 'angular.element'
+		angular[oldElement] = angular[newElement];
+
+		// Replace 'angular.element' with a new function
+		angular[newElement] = function( selector ) {
+
+			// The passed element is already an instance of angular._element.
+			if ( selector instanceof angular[oldElement] ) {
 				return selector;
 			}
 
-			var tmp;
+			// Ok, let's try the selector with Sizzle
 			if ( typeof selector === 'string' ) {
 
 				try {
-					tmp = Sizzle(selector);
+					var tmp = Sizzle(selector);
 
-					if( tmp.length === 0 ) {
-						tmp = selector;
+					if( tmp.length > 0 ) {
+						// Sizzle found something, pass it on to JQLite
+						return angular[oldElement](tmp);
 					}
 
 				} catch(err) {
 
-					if( angular.isDefined(ct.debug) ) {
-						console.error(err.message);
+					if( angular.isDefined(Sizzle.debug) ) {
+						console.error("Sizzle selector not found, message: ", err.message);
 					}
-					tmp = selector;
 
 				}
 
 			}
 
-			return angular[orginal](tmp);
+			// Ok, everything failed, lets JQLite sort the rest out.
+			return angular[oldElement](selector);
 
 		};
-	
+
+		// Loop through 'angular._element' and copy all objects to 'angular.element' (i.e. _data and data function)
+		for (var prop in angular[oldElement]) {
+			if (angular[oldElement].hasOwnProperty(prop)) {
+				angular[newElement][prop] = angular[oldElement][prop];
+			}
+		}
+
+
 	} else {
 		console.error('Angular-sizzle-standalone: Angular.js or Sizzle.js has not been defined');
 	}
-	
+
 })(angular, Sizzle);
