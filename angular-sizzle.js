f67da6a57e6bd7ba1b61521a60e78fcf216a6271
Paul de Vries <paul@qaraqter.nl>
2014-12-15 17:04:27 +0100

    Update inline documentation

--- angular-sizzle.js
+++ angular-sizzle.js
@@ -6,13 +6,13 @@
 
 	if(angular && sizzle) {
 
-		// Replace 'angular._element' with 'angular.element'
+		// Copy 'angular.element' to 'angular._element'
 		angular[oldElement] = angular[newElement];
 
 		// Replace 'angular.element' with a new function
 		angular[newElement] = function( selector ) {
 
-			// The passed element is already an instance of angular._element.
+			// The passed element is already an instance of 'angular._element'
 			if ( selector instanceof angular[oldElement] ) {
 				return selector;
 			}
@@ -21,15 +21,17 @@
 			if ( typeof selector === 'string' ) {
 
 				try {
+					// Sizzle it
 					var tmp = Sizzle(selector);
 
+					// Sizzle found something, pass it on to JQLite
 					if( tmp.length > 0 ) {
-						// Sizzle found something, pass it on to JQLite
 						return angular[oldElement](tmp);
 					}
 
 				} catch(err) {
 
+					// Log a message, define Sizzle.debug after loading Sizzle.js
 					if( angular.isDefined(Sizzle.debug) ) {
 						console.error("Sizzle selector not found, message: ", err.message);
 					}
@@ -38,12 +40,12 @@
 
 			}
 
-			// Ok, everything failed, lets JQLite sort the rest out.
+			// Everything failed, lets JQLite sort the rest out
 			return angular[oldElement](selector);
 
 		};
 
-		// Loop through 'angular._element' and copy all objects to 'angular.element' (i.e. _data and data function)
+		// Loop through 'angular._element' and copy all objects to 'angular.element' (i.e. _data and data functions)
 		for (var prop in angular[oldElement]) {
 			if (angular[oldElement].hasOwnProperty(prop)) {
 				angular[newElement][prop] = angular[oldElement][prop];
@@ -52,7 +54,7 @@
 
 
 	} else {
-		console.error('Angular-sizzle-standalone: Angular.js or Sizzle.js has not been defined');
+		console.error('angular-sizzle-standalone: Angular.js or Sizzle.js has not been defined');
 	}
 
 })(angular, Sizzle);
