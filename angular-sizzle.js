7d2277418c4f738ed49d0441b719a172525835d3
Paul de Vries <paul@qaraqter.nl>
2014-12-15 17:28:06 +0100

    Add a jQuery, Angular and Sizzle check before overwritting, also give developer feedback

--- angular-sizzle.js
+++ angular-sizzle.js
@@ -1,59 +1,71 @@
-(function (angular, sizzle) {
-    'use strict';
+(function (angular, sizzle, jQuery) {
+	'use strict';
 
-    var oldElement = '_element',
-        newElement = 'element';
+	var oldElement = '_element',
+	newElement = 'element';
 
-    if(angular && sizzle) {
+	// Check if angular is defined
+	if( angular === undefined || angular === null ) {
+		console.error('Whoooops, Angular was not found, load angular-sizzle-standalone after loading Angular.js and Sizzle.js');
+		return;
+	}
 
-        // Copy 'angular.element' to 'angular._element'
-        angular[oldElement] = angular[newElement];
+	// Check if jQuery is defined
+	if( angular.isDefined(jQuery) ) {
+		console.error('Whoooops, jQuery was found, you don\'t need jQuery in order to load Sizzle, Sizzle already ships within JQuery.');
+		return;
+	}
 
-        // Replace 'angular.element' with a new function
-        angular[newElement] = function( selector ) {
+	// Check if Sizzle is defined
+	if( !angular.isDefined(sizzle) ) {
+		console.error('Whoooops, Angular was not found, load angular-sizzle-standalone after loading Angular.js and Sizzle.js');
+		return;
+	}
 
-            // The passed element is already an instance of 'angular._element'
-            if ( selector instanceof angular[oldElement] ) {
-                return selector;
-            }
+	// Copy 'angular.element' to 'angular._element'
+	angular[oldElement] = angular[newElement];
 
-            // Ok, let's try the selector with Sizzle
-            if ( typeof selector === 'string' ) {
+	// Replace 'angular.element' with a new function
+	window.$ = angular[newElement] = function( selector ) {
 
-                try {
-                    // Sizzle it
-                    var tmp = Sizzle(selector);
+		// The passed element is already an instance of 'angular._element'
+		if ( selector instanceof angular[oldElement] ) {
+			return selector;
+		}
 
-                    // Sizzle found something, pass it on to JQLite
-                    if( tmp.length > 0 ) {
-                        return angular[oldElement](tmp);
-                    }
+		// Ok, let's try the selector with Sizzle
+		if ( typeof selector === 'string' ) {
 
-                } catch(err) {
+			try {
+				// Sizzle it
+				var tmp = Sizzle(selector);
 
-                    // Log a message, define Sizzle.debug after loading Sizzle.js
-                    if( angular.isDefined(Sizzle.debug) ) {
-                        console.error('Sizzle selector not found, message: ', err.message);
-                    }
+				// Sizzle found something, pass it on to JQLite
+				if( tmp.length > 0 ) {
+					return angular[oldElement](tmp);
+				}
 
-                }
+			} catch(err) {
 
-            }
+				// Log a message, define Sizzle.debug after loading Sizzle.js
+				if( angular.isDefined(Sizzle.debug) ) {
+					console.error('Sizzle selector not found, message: ', err.message);
+				}
 
-            // Everything failed, lets JQLite sort the rest out
-            return angular[oldElement](selector);
+			}
 
-        };
+		}
 
-        // Loop through 'angular._element' and copy all objects to 'angular.element' (i.e. _data and data functions)
-        for (var prop in angular[oldElement]) {
-            if (angular[oldElement].hasOwnProperty(prop)) {
-                angular[newElement][prop] = angular[oldElement][prop];
-            }
-        }
+		// Everything failed, lets JQLite sort the rest out
+		return angular[oldElement](selector);
 
-    } else {
-        console.error('angular-sizzle-standalone: Angular.js or Sizzle.js has not been defined');
-    }
+	};
+
+	// Loop through 'angular._element' and copy all objects to 'angular.element' (i.e. _data and data functions)
+	for (var prop in angular[oldElement]) {
+		if (angular[oldElement].hasOwnProperty(prop)) {
+			window.$ = angular[newElement][prop] = angular[oldElement][prop];
+		}
+	}
 
 })(angular, Sizzle);
